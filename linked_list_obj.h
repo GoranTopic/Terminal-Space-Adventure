3acbde1578c1690684c25a384fcc6df66b1dec64
telix <telegonicaxx@live.com>
2021-11-24 02:37:43 -0800

    lots of changes after a lot of time

--- linked_list_obj.h
+++ linked_list_obj.h
@@ -1,31 +1,31 @@
-    struct Linked_list_node* next;
-    struct Linked_list_node* prev;
-extern void list_remove_title(List* list, char* title);
-#ifndef LINKED_LIST_OBJ_H_
-extern void list_free(List* list);
-typedef struct Linked_list_node {
-extern void list_remove_at(List* list, int index);
-extern void node_free(Node* node);
-extern List* list_make(void);
-} List;
-#define LINKED_LIST_OBJ_H_
-extern Object* list_get_at(List* list, int index);
-} Node;
 #endif
-extern void list_print(List* list);
 extern Object* list_get_title(List* list, char* title);
-  /* Linked list for managing and storing objects */
-typedef struct Linked_list_head {
+typedef struct Linked_list_node {
+extern void node_free(Node* node);
+    struct Linked_list_node* prev;
+extern Object* list_get_at(List* list, int index);
 extern void list_append(List* list, Object* obj);
-  int count;
-  Node* Last;
-
-
-
-
-
-
-
-  /* a node for the linked list of objects*/
+#define LINKED_LIST_OBJ_H_
     Object* obj; //object which is held in the node
+#ifndef LINKED_LIST_OBJ_H_
+  /* a node for the linked list of objects*/
+extern void list_print(List* list);
+typedef struct Linked_list_head {
   Node* first;
+    struct Linked_list_node* next;
+extern void list_remove_title(List* list, char* title);
+  int count;
+extern void list_free(List* list);
+extern List* list_make(void);
+extern void list_remove_at(List* list, int index);
+} Node;
+
+
+
+
+
+
+
+  /* Linked list for managing and storing objects */
+  Node* Last;
+} List;
