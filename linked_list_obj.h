03926aefffa202ebbdc0b9ab9ae3473a7457ee4e
telix <telegonicaxx@live.com>
2021-11-24 02:38:39 -0800

    Out for vacation… DONT YOU DARE TO CALL ME.

--- linked_list_obj.h
+++ linked_list_obj.h
@@ -1,24 +1,19 @@
-#endif
-extern Object* list_get_title(List* list, char* title);
-typedef struct Linked_list_node {
-extern void node_free(Node* node);
-    struct Linked_list_node* prev;
-extern Object* list_get_at(List* list, int index);
 extern void list_append(List* list, Object* obj);
-#define LINKED_LIST_OBJ_H_
-    Object* obj; //object which is held in the node
-#ifndef LINKED_LIST_OBJ_H_
-  /* a node for the linked list of objects*/
+  /* Linked list for managing and storing objects */
+extern void list_free(List* list);
+    struct Linked_list_node* next;
+#endif
 extern void list_print(List* list);
 typedef struct Linked_list_head {
-  Node* first;
-    struct Linked_list_node* next;
-extern void list_remove_title(List* list, char* title);
-  int count;
-extern void list_free(List* list);
-extern List* list_make(void);
-extern void list_remove_at(List* list, int index);
 } Node;
+    struct Linked_list_node* prev;
+    Object* obj; //object which is held in the node
+extern Object* list_get_at(List* list, int index);
+extern void node_free(Node* node);
+#ifndef LINKED_LIST_OBJ_H_
+#define LINKED_LIST_OBJ_H_
+extern void list_remove_title(List* list, char* title);
+extern List* list_make(void);
 
 
 
@@ -26,6 +21,11 @@ extern void list_remove_at(List* list, int index);
 
 
 
-  /* Linked list for managing and storing objects */
-  Node* Last;
+typedef struct Linked_list_node {
+extern Object* list_get_title(List* list, char* title);
 } List;
+extern void list_remove_at(List* list, int index);
+  Node* Last;
+  int count;
+  Node* first;
+  /* a node for the linked list of objects*/
