1a41acdec1ccce68ea69b5f29a09893d86a7feca
telix <telegonicaxx@live.com>
2022-01-26 01:16:53 -0800

    this might break everything

--- objects.h
+++ objects.h
@@ -1,39 +1,39 @@
-#include <stdbool.h>
-#ifndef OBJECTS_H_
-#define OBJECTS_H_
-
-typedef struct Char_Point {
-    /* A point structure for saving the characters and cordinate of an object*/
-    int x, y;
-    char ch;
-} Point_ch;
-
-typedef struct Object {
-     /*A Structure for managin and renddering objects*/
-     Point_ch start; //starting point
-     char* title; //name of obj
-     char** model; //array of str that make out the obj
-     int model_c; // keep track of the number of line the drawing 
-     Point_ch* points; // char points
-     int points_c; //count of the char points
-     int speed;  // how a object moves
 		 char* type; //the type of the objec, ship, enemy, wall, bullet...etc
-		 int state; // the state on which the animation is at
+#define OBJECTS_H_
      bool point_left; //direction the obj move on
-     int life; // when it reaches 0 the obj is dead
-		 void* parent_list;
-} Object;
-
-	/*objecte fuctions*/
-extern Object* make_obj(char* title, char* model[], int model_c, int speed, Point_ch start, char* type);
-extern void free_obj(Object* obj);  
-extern void move_obj(Object* obj, int direction);
+		 int state; // the state on which the animation is at
 extern void kill_obj(Object* obj);
-extern void print_obj(Object* obj);
-extern void switch_direction(Object* obj);
-extern void reposition_obj(Object* obj, Point_ch new_start);
+extern Object* make_obj(char* title, char* model[], int model_c, int speed, Point_ch start, char* type);
+typedef struct Char_Point {
 
+
+
+
+
+     int model_c; // keep track of the number of line the drawing 
 	/*char pointer fuctions */
+typedef struct Object {
+extern void switch_direction(Object* obj);
+extern void move_obj(Object* obj, int direction);
 extern void print_point(Point_ch p);
-
+} Object;
+    char ch;
+     /*A Structure for managin and renddering objects*/
+	/*objecte fuctions*/
+     char** model; //array of str that make out the obj
+} Point_ch;
+    /* A point structure for saving the characters and cordinate of an object*/
+#include <stdbool.h>
+     int points_c; //count of the char points
+extern void print_obj(Object* obj);
+#ifndef OBJECTS_H_
+     char* title; //name of obj
 #endif
+    int x, y;
+extern void free_obj(Object* obj);  
+extern void reposition_obj(Object* obj, Point_ch new_start);
+     int speed;  // how a object moves
+     Point_ch* points; // char points
+		 void* parent_list;
+     Point_ch start; //starting point
+     int life; // when it reaches 0 the obj is dead
