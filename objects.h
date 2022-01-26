95cb43a548c200facfe590b2a7b31555c2f07b91
telix <telegonicaxx@live.com>
2022-01-26 01:17:53 -0800

    Crap. Tonight is raid night and I am already late.

--- objects.h
+++ objects.h
@@ -1,39 +1,39 @@
-		 char* type; //the type of the objec, ship, enemy, wall, bullet...etc
-#define OBJECTS_H_
      bool point_left; //direction the obj move on
-		 int state; // the state on which the animation is at
-extern void kill_obj(Object* obj);
-extern Object* make_obj(char* title, char* model[], int model_c, int speed, Point_ch start, char* type);
-typedef struct Char_Point {
-
-
-
-
-
-     int model_c; // keep track of the number of line the drawing 
-	/*char pointer fuctions */
-typedef struct Object {
-extern void switch_direction(Object* obj);
 extern void move_obj(Object* obj, int direction);
-extern void print_point(Point_ch p);
-} Object;
     char ch;
-     /*A Structure for managin and renddering objects*/
-	/*objecte fuctions*/
-     char** model; //array of str that make out the obj
-} Point_ch;
-    /* A point structure for saving the characters and cordinate of an object*/
-#include <stdbool.h>
-     int points_c; //count of the char points
-extern void print_obj(Object* obj);
-#ifndef OBJECTS_H_
-     char* title; //name of obj
-#endif
-    int x, y;
-extern void free_obj(Object* obj);  
-extern void reposition_obj(Object* obj, Point_ch new_start);
-     int speed;  // how a object moves
-     Point_ch* points; // char points
-		 void* parent_list;
-     Point_ch start; //starting point
      int life; // when it reaches 0 the obj is dead
+		 char* type; //the type of the objec, ship, enemy, wall, bullet...etc
+} Point_ch;
+     Point_ch* points; // char points
+     char* title; //name of obj
+#include <stdbool.h>
+typedef struct Char_Point {
+#endif
+extern void kill_obj(Object* obj);
+     int points_c; //count of the char points
+extern void reposition_obj(Object* obj, Point_ch new_start);
+	/*char pointer fuctions */
+
+
+
+
+
+		 void* parent_list;
+    int x, y;
+     char** model; //array of str that make out the obj
+#ifndef OBJECTS_H_
+extern void print_point(Point_ch p);
+     int speed;  // how a object moves
+extern void free_obj(Object* obj);  
+#define OBJECTS_H_
+typedef struct Object {
+     Point_ch start; //starting point
+extern void print_obj(Object* obj);
+} Object;
+extern Object* make_obj(char* title, char* model[], int model_c, int speed, Point_ch start, char* type);
+    /* A point structure for saving the characters and cordinate of an object*/
+extern void switch_direction(Object* obj);
+     int model_c; // keep track of the number of line the drawing 
+	/*objecte fuctions*/
+		 int state; // the state on which the animation is at
+     /*A Structure for managin and renddering objects*/
