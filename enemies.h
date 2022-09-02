327703da0c25aa467fb05a7a85615fbcb6d29d65
telix <telegonicaxx@live.com>
2022-09-02 03:16:44 -0700

    Please our Lord and Savior the Great Linter.

--- enemies.h
+++ enemies.h
@@ -1,13 +1,13 @@
-#include "array_obj.h"
-#ifndef ENEMIES_H_
-#define ENEMIES_H_
-
-/*
-	 functions for defining and loading enemies
- */
-
-extern Obj_arr* load_enemies_ar(void);
-extern Object* random_enemy(int max_x, int max_y, Obj_arr* enemies);
-
-
 #endif
+extern Object* random_enemy(int max_x, int max_y, Obj_arr* enemies);
+	 functions for defining and loading enemies
+/*
+
+
+
+
+ */
+#include "array_obj.h"
+extern Obj_arr* load_enemies_ar(void);
+#define ENEMIES_H_
+#ifndef ENEMIES_H_
