1d74eca85b2c1fc806261e41c5a991e11566b927
telix <telegonicaxx@live.com>
2022-10-25 01:24:00 -0700

    about to structure of code

--- flxarray.c
+++ flxarray.c
@@ -1,102 +1,102 @@
-	int *items;
-		//struc fot flxarrays
-	int capacity;
-		// add items from working to array
-void flxarray_free(flxarray fa) {
-	int i = 0, j = len / 2, c = 0;
-#include <stdio.h>
-	}	
-	//print areray evenly and nicely =D
-	int working[size];
-	};
-		printf("FA[%3d] = %5d\n", i, fa->items[i]);
-	mergesort(fa->items, fa->count);
-	free(fa);
-	result->items = emalloc(result->capacity * sizeof result->items[0]);
-int merge_sort_recursive(int* array, int* w , int len){
-	}
-	
-	
-	
-	
-	
-	
-	while( i < len / 2) w[c++] = a[i++];
-	return EXIT_SUCCESS;
-	return EXIT_SUCCESS;
-		}
-		
-		
-			fa->items = erealloc(fa->items, fa->capacity * sizeof fa->items[0]);
-}
-}
-}
-}
-}
-}
-}
-			
-#include <stdlib.h>
-	while(i < len/ 2 && j < len){
-	if(len < 2) return 0;
-	/* mergesort would be good */
-#include "mylib.h"
-			fa->capacity += fa->capacity;
-	result->capacity = 2;
-	merge(array, w, len);
-	while( j < len) w[c++] = a[j++];
-	flxarray result = emalloc(sizeof *result);
- //mergesort recursibly
-		//while there still elements in j
-void merge(int* a, int* w, int len){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	result->count = 0;
-	merge_sort_recursive(array, w, len / 2); // divide left side
-	if (fa->count == fa->capacity) {
-flxarray flxarray_new() {
 /* insert the item in the last free space */
-void flxarray_sort(flxarray fa) {
-	for(int i = 0; i < len; i++) array[i] = w[i];
-struct flxarrayrec { //why does it have rec at the en of the struct name?
-	/* a "for" loop to print out each cell of f->items */
-	merge_sort_recursive(array, working, size);
-} 
-#include "flxarray.h"
-	//merges the two halfs 
-	fa->items[fa->count++] = num; 
+	}	
 				/* do the old "double the capacity" trick */
-	merge_sort_recursive(array + (len / 2), w, len - (len / 2)); //divide right side 
-void flxarray_append(flxarray fa, int num) {
-		//while there still elements in i
-	for(int i = 0; i < fa->count; i++){
-int mergesort(int* array, int size){
-	int count;
- 
-		if( a[i] < a[j] ) w[c++] = a[i++];
-		// fl
-		else w[c++] = a[j++];
-/* free the memory associated with the flexarray */
-return result;
+	while(i < len/ 2 && j < len){
 	printf("flxarray count: %3d\n", fa->count);
+return result;
+		if( a[i] < a[j] ) w[c++] = a[i++];
+	};
+			fa->capacity += fa->capacity;
+	int working[size];
+#include "flxarray.h"
+	if (fa->count == fa->capacity) {
+	result->capacity = 2;
+	while( j < len) w[c++] = a[j++];
+ 
+	
+	
+	
+	
+	
+	
+ //mergesort recursibly
+/* free the memory associated with the flexarray */
+} 
+		// add items from working to array
+#include "mylib.h"
+	merge_sort_recursive(array + (len / 2), w, len - (len / 2)); //divide right side 
+int mergesort(int* array, int size){
+struct flxarrayrec { //why does it have rec at the en of the struct name?
+	int *items;
+#include <stdio.h>
+void flxarray_append(flxarray fa, int num) {
+		else w[c++] = a[j++];
+	free(fa);
+	fa->items[fa->count++] = num; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void merge(int* a, int* w, int len){
+}
+}
+}
+}
+}
+}
+}
+#include <stdlib.h>
+	result->count = 0;
+	result->items = emalloc(result->capacity * sizeof result->items[0]);
+		//while there still elements in i
+		printf("FA[%3d] = %5d\n", i, fa->items[i]);
 void flxarray_print(flxarray fa) {
+		//while there still elements in j
+		//struc fot flxarrays
+	merge_sort_recursive(array, working, size);
+void flxarray_sort(flxarray fa) {
+	//print areray evenly and nicely =D
+	int i = 0, j = len / 2, c = 0;
+			
+	}
+	/* mergesort would be good */
+	int capacity;
+	if(len < 2) return 0;
+	mergesort(fa->items, fa->count);
+	merge_sort_recursive(array, w, len / 2); // divide left side
+	int count;
+void flxarray_free(flxarray fa) {
+			fa->items = erealloc(fa->items, fa->capacity * sizeof fa->items[0]);
+		
+		
+	merge(array, w, len);
+		}
+		// fl
+	for(int i = 0; i < fa->count; i++){
+	//merges the two halfs 
+	/* a "for" loop to print out each cell of f->items */
+int merge_sort_recursive(int* array, int* w , int len){
+	while( i < len / 2) w[c++] = a[i++];
+	flxarray result = emalloc(sizeof *result);
+	return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
+	for(int i = 0; i < len; i++) array[i] = w[i];
+flxarray flxarray_new() {
